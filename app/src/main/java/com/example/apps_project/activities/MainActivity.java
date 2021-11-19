b76d9673c148640050c564bfb49b242b318cf17c
manuelcastano <manuelsabe186@gmail.com>
2021-11-19 11:48:32 -0500

    recuperacion de login

--- app/src/main/java/com/example/apps_project/activities/MainActivity.java
+++ app/src/main/java/com/example/apps_project/activities/MainActivity.java
@@ -8,21 +8,29 @@ import android.os.Bundle;
 import com.example.apps_project.R;
 import com.example.apps_project.model.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private User loadedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User loadedUser = null;
         if(FirebaseAuth.getInstance().getCurrentUser() == null || !FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
             Intent intent = new Intent(this, RolActivity.class);
             startActivity(intent);
             finish();
             return;
         }else{
+            FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).get().addOnCompleteListener(
+                    task ->{
+                        loadedUser = task.getResult().toObject(User.class);
+                    }
+            );
             switch (loadedUser.getRol()){
                 case "client":
                     Intent intent = new Intent(this, RolActivity.class);
